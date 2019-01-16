81c5caf56d8bc4c01a6fe16fa2c5e28234a8fdea
Hans Erik Heggem <heh@dips.no>
2019-01-16 14:51:52 +0100

    updated dependencies

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.21',  # Required
+    version='1.1.22',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -133,7 +133,8 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['DockerBuildSystem', 'pyyaml'],  # Optional
+    install_requires=[
+        'DockerBuildSystem>=1.1.20'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
