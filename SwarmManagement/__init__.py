110af9a855f984d09ad199e34d1568b4dd75f28e
Hans Erik Heggem <heh@dips.no>
2019-04-16 11:46:03 +0200

    updated application entrypoint

--- SwarmManagement/__init__.py
+++ SwarmManagement/__init__.py
@@ -1,20 +1,12 @@
 from SwarmManagement import SwarmManager
 import sys
 
-def RemoveInvalidArguments(arguments):
-    if len(arguments) == 0:
-        return arguments
-    elif arguments[0] == 'python' or '.py' in arguments[0]:
-        arguments = arguments[1:]
-        arguments = RemoveInvalidArguments(arguments)
-    return arguments
-
 def main():
     """Entry point for the application script"""
     arguments = sys.argv[1:]
-    print('Managing Swarm with arguments: ')
+    print('Managing solution with arguments: ')
     print(arguments)
-    arguments = RemoveInvalidArguments(arguments)
-    if len(arguments) == 0:
-        arguments = ['-help']
     SwarmManager.HandleManagement(arguments)
+
+if __name__ == "__main__":
+    main()
