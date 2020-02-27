6970384788a9b23f9bda921ee58ad45c5a750d48
Hans Erik Heggem <heh@dips.no>
2020-02-27 11:45:42 +0100

    removed some prints

--- SwarmManagement/__main__.py
+++ SwarmManagement/__main__.py
@@ -4,8 +4,6 @@ import sys
 def main():
     """Entry point for the application script"""
     arguments = sys.argv[1:]
-    print('Managing solution with arguments: ')
-    print(arguments)
     SwarmManager.HandleManagement(arguments)
 
 if __name__ == "__main__":
